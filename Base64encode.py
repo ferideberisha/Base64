0a4a151e1e3a64882e606dde6dae9198a20b4e27
FFazliu <95186814+FFazliu@users.noreply.github.com>
2022-05-15 21:27:07 +0200

    Update Base64encode.py

--- Base64encode.py
+++ Base64encode.py
@@ -9,3 +9,18 @@ class Base64(object):
         if len(data) % 3 != 0:
             override = (len(data) + 3 - len(data) % 3) - len(data)
         data += b"\x00"*override
+        threechunks = self.chunk(data, 3)
+
+        binstring = ""
+        for chunk in threechunks:
+            for x in chunk:
+                binstring += "{:0>8}".format(bin(x)[2:])
+
+        sixchunks = self.chunk(binstring, 6)
+
+        outstring = ""
+        for element in sixchunks:
+            outstring += self.CHARS[int(element, 2)]
+
+        outstring = outstring[:-override] + "="*override
+        return outstring
