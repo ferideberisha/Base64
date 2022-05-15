848960a661d6d2e219c2cd181deafe23e550b411
festinaimeraj <93921647+festinaimeraj@users.noreply.github.com>
2022-05-15 23:49:59 +0200

    Update Base64Decode.java

--- Base64Decode.java
+++ Base64Decode.java
@@ -6,3 +6,14 @@ class Base64Decode {
         // remove/ignore any characters not in the base64 characters list
         // or the pad character -- particularly newlines
         s = s.replaceAll("[^" + base64chars + "=]", "");
+  // replace any incoming padding with a zero pad (the 'A' character is
+        // zero)
+        String p = (s.charAt(s.length() - 1) == '=' ?
+                (s.charAt(s.length() - 2) == '=' ? "AA" : "A") : "");
+        String r = "";
+        s = s.substring(0, s.length() - p.length()) + p;
+
+        // increment over the length of this encoded string, four characters
+        // at a time
+        for (int c = 0; c < s.length(); c += 4) {
+            
