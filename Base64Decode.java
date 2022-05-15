c2319fcfa22efb6499c5efbf368e785b03fa2172
FFazliu <95186814+FFazliu@users.noreply.github.com>
2022-05-15 23:50:55 +0200

    Update Base64Decode.java

--- Base64Decode.java
+++ Base64Decode.java
@@ -16,4 +16,21 @@ class Base64Decode {
         // increment over the length of this encoded string, four characters
         // at a time
         for (int c = 0; c < s.length(); c += 4) {
-            
+            // each of these four characters represents a 6-bit index in the
+            // base64 characters list which, when concatenated, will give the
+            // 24-bit number for the original 3 characters
+            int n = (base64chars.indexOf(s.charAt(c)) << 18)
+                    + (base64chars.indexOf(s.charAt(c + 1)) << 12)
+                    + (base64chars.indexOf(s.charAt(c + 2)) << 6)
+                    + base64chars.indexOf(s.charAt(c + 3));
+
+            // split the 24-bit number into the original three 8-bit (ASCII)
+            // characters
+            r += "" + (char) ((n >>> 16) & 0xFF) + (char) ((n >>> 8) & 0xFF)
+                    + (char) (n & 0xFF);
+        }
+
+        // remove any zero pad that was added to make this a multiple of 24 bits
+        return r.substring(0, r.length() - p.length());
+    }
+}       
